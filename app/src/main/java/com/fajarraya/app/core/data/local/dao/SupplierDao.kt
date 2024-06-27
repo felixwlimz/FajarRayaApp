11b1d8cc6beb52354d8832ed910e4c5d4aec0654
Felix Winston <felixlim140@gmail.com>
2024-06-27 12:32:41 +0700

    Fixed SupplierDao with no SQL functions

--- app/src/main/java/com/fajarraya/app/core/data/local/dao/SupplierDao.kt
+++ app/src/main/java/com/fajarraya/app/core/data/local/dao/SupplierDao.kt
@@ -1,19 +1,31 @@
 package com.fajarraya.app.core.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import com.fajarraya.app.core.data.local.entity.SupplierEntity
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Flowable
 
+@Dao
 interface SupplierDao {
 
+    @Query("SELECT * FROM suppliers")
     fun getAllSuppliers() : Flowable<List<SupplierEntity>>
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addSupplier(supplierEntity: SupplierEntity) : Completable
 
+    @Update
     fun updateSupplier(supplierEntity: SupplierEntity) : Completable
 
+    @Delete
     fun deleteSupplier(supplierEntity: SupplierEntity) : Completable
 
+    @Query("SELECT * FROM suppliers WHERE supplier_id = :supplierId")
     fun getSupplier(supplierId : String) : Flowable<SupplierEntity>
 
 }
---
+++ app/src/main/java/com/fajarraya/app/core/data/local/dao/TransactionDao.kt
@@ -0,0 +1,7 @@
+package com.fajarraya.app.core.data.local.dao
+
+import androidx.room.Dao
+
+@Dao
+interface TransactionDao {
+}
